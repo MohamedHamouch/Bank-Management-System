e02b24bed8d7f72ccf79a3e5fe5948acec2d5e7e
Mohamed Hamouch <med10hamouch@gmail.com>
2025-09-19 11:54:52 +0100

    create abstract class operation

---
+++ src/models/Operation.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.UUID;
+import java.time.LocalDate;
+
+public abstract class Operation {
+    protected UUID number;
+    protected LocalDate date;
+    protected double amount;
+
+    public Operation(double amount) {
+        this.number = UUID.randomUUID();
+        this.amount = amount;
+        this.date = LocalDate.now();
+    }
+
+    public Operation() {
+        this.number = UUID.randomUUID();
+        this.amount = 0;
+        this.date = LocalDate.now();
+    }
+
+    public UUID getNumber() {
+        return number;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
